753cf9e5c41aff2c8db2c5dafa1e56411154e922
yuanbohan <yuanbo.han@gmail.com>
2023-10-20 12:00:26 +0800

    tree-sitter package

--- init.el
+++ init.el
@@ -20,6 +20,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(sanityinc-tomorrow-eighties))
+ '(custom-safe-themes
+   '("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))
  '(package-selected-packages '(browse-at-remote)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.

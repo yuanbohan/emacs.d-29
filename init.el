c5469d9d95672831b14cc6ee3ddc70827c6627cb
yuanbohan <yuanbo.han@gmail.com>
2023-11-10 10:48:43 +0800

    chore: update key

--- init.el
+++ init.el
@@ -4,12 +4,15 @@
 
 ;; Define package repositories
 (require 'package)
- (add-to-list 'package-archives
-         '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
 ;; (add-to-list 'package-archives
 ;;              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives
-         '("gnu-devel" . "https://elpa.gnu.org/devel/") t)
+
+
+(setq url-proxy-services '(("no_proxy" . "localhost, 127.0.0.1")
+                           ("http" . "http://127.0.0.1:7890")
+                           ("https" . "https://127.0.0.1:7890")))
 
 (package-initialize)
 
@@ -24,7 +27,7 @@
  '(custom-safe-themes
    '("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))
  '(package-selected-packages
-   '(yaml-imenu which-key toml-mode markdown-mode browse-at-remote)))
+   '(which-key browse-at-remote yasnippet-snippets whitespace-cleanup-mode treemacs tree-sitter-langs smartparens rust-mode multiple-cursors magit go-mode f expand-region counsel-tramp company color-theme-sanityinc-tomorrow)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
